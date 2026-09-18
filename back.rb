08852863fe963153354b3fdc0ff5a5915bc73e6a
Taiki Katsube <j2313@matsue.ct.ac.jp>
2026-09-18 22:31:10 +0900

    背景画像を変更し、新しい画像ファイルを追加

--- back.rb
+++ back.rb
@@ -2,15 +2,15 @@ class Back
     def initialize(window)
         @image = Array.new(5)
         @image[0] = Gosu::Image.new("media/back/Sora.png")
-        @image[1] = Gosu::Image.new("media/back/Sora.png")
+        @image[1] = Gosu::Image.new("media/back/kojo.png")
         @image[2] = Gosu::Image.new("media/back/Sora.png")
-        @image[3] = Gosu::Image.new("media/back/Sora.png")
-        @image[4] = Gosu::Image.new("media/back/Sora.png")
+        @image[3] = Gosu::Image.new("media/back/kori.png")
+        @image[4] = Gosu::Image.new("media/back/maguma.png")
 
         @x = @y = 0.0
         @xmax = 59
         @ymax = 9
-        @stagemum = 4
+        @stagemum = 1
 
         input_back    # マップとタイル画像はinitializeで一度だけ読み込む
         input_tiles
@@ -111,7 +111,7 @@ class Back
     end
 
     def draw(x, y) #背景の描画
-        @image[0].draw(0, 0, -1)
+        @image[@stagemum-1].draw(0, 0, -1)
         if @stagemum == 1
             @map0.each_with_index do |row, i|
                 row.each_with_index do |tile_num, j|
@@ -149,7 +149,7 @@ class Back
                 row.each_with_index do |tile_num, j|
                     tile = @tiles[tile_num]
                     next if tile.nil?
-                    tile.draw(x + j * 50, y + i * 50 - 20, 1)
+                    tile.draw(x + j * 50, y + i * 50 - 20, 0)
                 end
             end
         end
