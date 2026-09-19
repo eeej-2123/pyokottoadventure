fd2d17c10449fb77330d7afd0c8737bc2c945788
Taiki Katsube <j2313@matsue.ct.ac.jp>
2026-09-19 12:10:27 +0900

    ステージの初期値を1に変更し、setメソッドを追加

--- back.rb
+++ back.rb
@@ -20,11 +20,15 @@ class Back
         @x = @y = 0.0
         @xmax = 59
         @ymax = 9
-        @stagemum = 6
+        @stagemum = 1
         @sound_size = 1.0   # 100 → 1.0 に変更
         input_back    # マップとタイル画像はinitializeで一度だけ読み込む
         input_tiles
         start_bgm(0)
+    end
+
+    def set
+        @stagemum = 1
     end
 
     def get_stagemum
